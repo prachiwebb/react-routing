bb87e62821e22fea19e553bc83f3e7b795af89aa
prachiwebb <prachiwebb@gmail.com>
2020-06-22 15:41:59 +0530

    react routing

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+
+const About = () => {
+	return (
+		<div className= "Aboutstyle">
+		<h1>Welcome to About page</h1>
+		</div>
+
+
+		)
+}
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.css';
+
+
+const Contact = () => {
+	return (
+		<div className= "Contactstyle">
+		<h1>Welcome to Contact page</h1>
+		</div>
+
+
+		)
+}
+export default Contact;
